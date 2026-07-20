84567574ca18d7506388f610a82ba70109dfa949
X_HRISHAV <rajhrishav253@gmail.com>
2026-07-20 21:15:40 +0530

    Add rank transform function for an array
    Implement rank transformation for an array using sorting and a hash map.

---
+++ ARRAY/1331. Rank Transform of an Array.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<int> arrayRankTransform(vector<int>& arr) {
+        vector<int> temp = arr;
+        sort(temp.begin(), temp.end());
+
+        unordered_map<int, int> rank;
+        int r = 1;
+
+        for (int num : temp) {
+            if (rank.find(num) == rank.end()) {
+                rank[num] = r;
+                r++;
+            }
+        }
+
+        for (int i = 0; i < arr.size(); i++) {
+            arr[i] = rank[arr[i]];
+        }
+
+        return arr;
+    }
+};
